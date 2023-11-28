c0a2a6dc2651a4a4d1869e396b53dd422819c634
Your Name <you@example.com>
2023-11-28 11:31:54 +0600

    practic

---
+++ main.js
@@ -0,0 +1,26 @@
+// let number = [1, 2, 3]
+
+// number.forEach((el) => {
+//     if (el == 2)
+//     console.log(el=10);
+// else{
+//     console.log(el);
+// }
+// })
+
+
+
+
+
+
+// console.log(number);
+
+let array = [9, 20, 7, 12, 39, 6, 1]
+array.forEach((el) => {
+    if (el >= 10)
+        console.log(el);
+})
+
+console.log(array);
+ 
+    
